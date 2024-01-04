d61aeacd6f247fac397891a31cf766eed84726f4
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-04 16:50:25 -0500

    null-undefined

---
+++ src/10-null-undefined.ts
@@ -0,0 +1,22 @@
+(()=>{
+  let myVar = null; // Tipo any
+  console.log(typeof myVar);
+  myVar = undefined; // Tipo any
+  console.log(typeof myVar);
+
+  let myNull: null=null
+  console.log(typeof myNull);
+  let myUndefined: undefined=undefined
+  console.log(typeof myUndefined);
+
+
+  let myNumber: number | null = null;
+  myNumber = 10;
+  console.log(myNumber);
+  let myString: string | undefined=undefined
+  myString = '10';
+  console.log(myString);
+  myString = undefined
+  console.log(myString);
+
+})()
